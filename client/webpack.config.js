e6e8c266218e5f6517825204f9520a573b9b6db1
Diana Vu <aznalive@hotmail.com>
2022-11-11 11:56:27 -0500

    added in starter code aswell as webpacks and manifest into webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      //Generates our html and injects our bundle
+      new HtmlWebpackPlugin({
+        template: "/.index.html",
+        title: "Text Editor",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+    ],
+
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
+};
