cab72483216943929527a2604131d3cc56a40638
Diana Vu <aznalive@hotmail.com>
2022-11-14 14:43:10 -0500

    added rules into webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,11 +24,48 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "Neat editor that allows online and offline",
+        background_color: "#272822",
+				theme_color: "#272822",
+				start_url: "./",
+				publicPath: "./",
+        icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("src", "images", "logo.png"),
+					},
+        ],
+
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+           test: /\.css$/i,
+           use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+             loader: "babel-loader",
+             options: {
+               presets: ["@babel/preset-env"],
+               plugins: [
+                 "@babel/plugin-proposal-object-rest-spread",
+                 "@babel/transform-runtime",
+               ],
+             },
+          },
+        },
       ],
     },
   };
